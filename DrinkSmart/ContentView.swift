7414cdc907a65d690d812f3c6a981a5a6e904ced
Rafael Oliveira <rafaelismael@hotmail.com>
2020-10-30 18:44:11 -0400

    Initial Commit

---
+++ DrinkSmart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DrinkSmart
+//
+//  Created by Rafael Oliveira on 2020-10-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
