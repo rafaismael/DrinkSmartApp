f6c0e974d6539fcf8aed93f6cdd073450eef41c3
Rafael Oliveira <rafaelismael@hotmail.com>
2020-11-02 16:59:54 -0500

    Editing DrinkView() - Type

--- DrinkSmart/DrinksType.swift
+++ DrinkSmart/DrinksType.swift
@@ -8,10 +8,43 @@
 import SwiftUI
 
 struct DrinksType: View {
+    
+    @State private var filterDrink = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+      
+        
+        VStack{
+            Image("waveadd")
+                .resizable()
+                .frame(width:.infinity, height: 200)
+                .offset(x: 0, y: -190)
+                .edgesIgnoringSafeArea(.horizontal)
+            
+            TextField("Procurar", text: $filterDrink).offset(x: 0, y: -260) .textFieldStyle(RoundedBorderTextFieldStyle()).frame(width:330)
+            
+            HStack{
+                Button(action: {
+                    // add water
+                }){ Image("water").resizable().frame(width: 50, height:75).padding()}
+                
+                Button(action: {
+                    // add water
+                }){Image("redwine").resizable().frame(width: 50, height:75).padding()}
+                
+                Button(action: {
+                    // add water
+                }){Image("coffee").resizable().frame(width: 50, height:75).padding()}
+                
+                Button(action: {
+                    // add water
+                }){Image("beer").resizable().frame(width: 50, height:75).padding()}
+            }.offset(x: 0, y: -200)
+        }
+            
+        
+        
+}}
 
 struct DrinksType_Previews: PreviewProvider {
     static var previews: some View {
